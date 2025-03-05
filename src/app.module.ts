47d49cd4362b8a083708ab458f1350e8916f2532
Neri Uriel Sosa Villanueva <sosavillanuevan@gmail.com>
2025-03-05 02:29:37 -0600

    Docker Compose de la BD realizado junto con la configuracion para el uso de variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT', 5432),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(new ValidationPipe(
+    { transform: true, whitelist: true, forbidNonWhitelisted: true },
+  ));
+  app.enableCors();
+
+  const port = configService.get('PORT', 3000);
+  await app.listen(port);
+  console.log(`Application running on port ${port}`);
 }
 bootstrap();
