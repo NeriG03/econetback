efff8926c089d4a61c82d1454461c70424797a85
Neri Uriel Sosa Villanueva <sosavillanuevan@gmail.com>
2025-03-26 01:56:58 -0600

    Importacion de ambas entidades una en la otra para correcto uso de relaciones

--- src/notices/notices.module.ts
+++ src/notices/notices.module.ts
@@ -3,9 +3,10 @@ import { NoticesService } from './notices.service';
 import { NoticesController } from './notices.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Notice } from './entities/notice.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Notice])],
+  imports: [TypeOrmModule.forFeature([Notice, User])],
   controllers: [NoticesController],
   providers: [NoticesService],
 })
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
+import { Notice } from 'src/notices/entities/notice.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Notice])],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
